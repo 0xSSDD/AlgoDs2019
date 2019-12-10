8ffccf403c9f936573bcd180419a0c0aaf95a1e7
smalldutta <49087443+smalldutta@users.noreply.github.com>
2019-12-10 19:33:24 +0200

    Factorial of a number using recursion

--- Main.java
+++ Main.java
@@ -1,23 +1,16 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
-        LinkList theList = new LinkList();
-        theList.insertFirst(22, 2.99);
-        theList.insertFirst(44, 4.99);
-        theList.insertFirst(66, 6.99);
-        theList.insertFirst(88, 8.99);
-        theList.toString();
-
-        while( !theList.isEmpty() ) {
-
-            Link aLink = theList.deleteFirst();
-            System.out.print( "Deleted");
-            aLink.toString();
-            System.out.println("");
-        }
-
-        theList.toString();
+        int num = 6;
+        float factorial = multiplyNumbers(num);
+        System.out.println("Factorial of " + num + " = " + factorial);
+    }
+    public static float multiplyNumbers(int num)
+    {
+        if (num >= 1)
+            return num * multiplyNumbers(num - 1);
+        else
+            return 1;
     }
 }
